9aca4acddfbf4e78343f880765532336445e3ccb
Philipp Saboi <ph.saboi@gmail.com>
2024-07-20 17:57:29 +0200

    added extra hidden layer

--- try.py
+++ try.py
@@ -7,14 +7,17 @@ class Neural_Network:
     def __init__(self, X, y, learning_rate, maximal_loss):
         np.random.seed(0)
         self.input_size = len(X[0])
-        self.hidden_size = 100
+        self.hidden_lay1_size = 100
+        self.hidden_lay2_size = 100
         self.output_size = len(y[0])
-        self.W1 = np.random.randn(self.input_size, self.hidden_size)
-        self.W2 = np.random.randn(self.hidden_size, self.output_size)
+        self.W1 = np.random.randn(self.input_size, self.hidden_lay1_size)
+        self.W2 = np.random.randn(self.hidden_lay1_size, self.hidden_lay2_size)
+        self.W3 = np.random.randn(self.hidden_lay2_size, self.output_size)
         self.X = X
         self.y = y
         self.path_w2 = os.path.dirname(__file__)+ '/W2.txt'
         self.path_w1 = os.path.dirname(__file__)+ '/W1.txt'
+        self.path_w3 = os.path.dirname(__file__)+ '/W3.txt'
         self.learning_rate = learning_rate 
         self.maximal_loss = maximal_loss
         self.pyplot_xaxis = []
@@ -26,18 +29,16 @@ class Neural_Network:
     def ddx_sigmoid(x):
         return x * (1 - x)
 
-    def relu(x):
-        return x
-
     # Definiere die Verwärtspropagation def forward(self):
     def forward(self):
         z1 = np.dot(self.X, self.W1) #to understand X * W1 = z1, sigmoid(z1) = a1, a1 * W2 = z2, sigmoid(z2) = a2, a2 * W3 = z3, sigmoid(z3) = a3
         a1 = Neural_Network.sigmoid(z1)
         z2 = np.dot(a1, self.W2)
-        self.y_pred = Neural_Network.sigmoid(z2)
+        a2 = Neural_Network.sigmoid(z2)
+        z3 = np.dot(a2, self.W3)
+        self.y_pred = Neural_Network.sigmoid(z3)
 
-
-        return self.y_pred, a1
+        return self.y_pred, a1, a2
 
     # Definiere die Verlustfunktion (hier verwenden wir den mittleren quadratischen Fehler)
     def loss (self):
@@ -53,28 +54,31 @@ class Neural_Network:
 
             while(self.l > self.maximal_loss):
                 #Vorwärtspropagation
-                self.y_pred, a1 = Neural_Network.forward(self)
+                self.y_pred, a1, a2 = Neural_Network.forward(self)
         
                 #Berechne den VerLust
                 self.l = Neural_Network.loss(self)
 
-                #berechne den Gradienten der Verlustfunktion
-        
+                
+                #backpropagation
                 grad_y_pred = 2 * (self.y_pred - self.y)
-                grad_z2 = grad_y_pred * Neural_Network.ddx_sigmoid(self.y_pred)#self.y_pred * (1 - self.y_pred)
+                grad_z3 = grad_y_pred * Neural_Network.ddx_sigmoid(self.y_pred)
+                grad_W3 = np.dot(a2.T, grad_z3)
+
+                grad_a2 = np.dot(grad_z3, self.W3.T)
+                grad_z2 = grad_a2 * Neural_Network.ddx_sigmoid(a2) #grad_z2 = 
                 grad_W2 = np.dot(a1.T, grad_z2)
     
                 grad_a1 = np.dot(grad_z2, self.W2.T)
                 grad_z1 = grad_a1 * Neural_Network.ddx_sigmoid(a1)
                 grad_W1 = np. dot(self.X.T, grad_z1)
 
-                
-
                 # Aktualisiere die Gewichte und den Bias mithilfe des Gradientenabstiegs
                 self.W1 -= self.learning_rate * grad_W1
-
                 self.W2 -= self.learning_rate * grad_W2
+                self.W3 -= self.learning_rate * grad_W3
 
+                #plot learning behaviour
                 self.pyplot_xaxis.append(iteration)
                 self.pyplot_yaxis.append(self.l)
 
@@ -96,18 +100,25 @@ class Neural_Network:
                 if i != 0:
                     file.write('\n')
                 for j in range(0, len(self.W1[i])):
-                    file.write(f'{str(self.W1[i][j])}')
+                    file.write(f'{str(self.W1[i][j])}' + ' ')
 
         with open(self.path_w2, 'w') as file:
             for i in range (0,len(self.W2)):
                 if i !=0:
                     file.write('\n')
                 for j in range(0, len(self.W2[i])):
-                    file.write(f'{str(self.W2[1][j])}')
+                    file.write(f'{str(self.W2[1][j])}' + ' ')
+
+        with open(self.path_w3, 'w') as file:
+            for i in range (0,len(self.W3)):
+                if i !=0:
+                    file.write('\n')
+                for j in range(0, len(self.W3[i])):
+                    file.write(f'{str(self.W3[1][j])}' + ' ')
         
         print (f'Genome saved @: {self.path_w1} and {self.path_w2} modelcode: axd1f')
         
-        return self.W1, self.W2
+        return self.W1, self.W2, self.W3
 
 
     def call_trained_model (self):
@@ -115,7 +126,7 @@ class Neural_Network:
         print ('calling model weights')
         #call trained model:
         with open (self.path_w1, 'r') as file: 
-            W1 = np.empty((self.input_size, self.hidden_size))
+            self.W1 = np.empty((self.input_size, self.hidden_lay1_size))
             for i, line in enumerate (file):
                 line = line.split(' ')
                 line. pop (-1)
@@ -123,12 +134,20 @@ class Neural_Network:
                     self.W1[i][j] = float(weight)
 
         with open (self.path_w2, 'r') as file: 
-            W2 = np.empty((self.input_size, self.hidden_size))
+            self.W2 = np.empty((self.hidden_lay1_size, self.hidden_lay2_size))
             for i, line in enumerate (file):
                 line = line.split(' ')
                 line. pop (-1)
                 for j, weight in enumerate(line):
                     self.W2[i][j] = float(weight)
+
+        with open (self.path_w3, 'r') as file: 
+            self.W3 = np.empty((self.hidden_lay2_size, self.output_size))
+            for i, line in enumerate (file):
+                line = line.split(' ')
+                line. pop (-1)
+                for j, weight in enumerate(line):
+                    self.W3[i][j] = float(weight)
 
         print (f'Genome called @: {self.path_w1} and {self.path_w2} modelcode: axdif*)')
 
@@ -183,11 +202,10 @@ class Neural_Network:
 
 if __name__ == '__main__':
 
-
-    X = np. array([[2, 0], [4, 1], [3, 1], [4, 6]])
+    X = np. array([[2, 6], [4, 1], [3, 1], [5, 6]])
     y = np.array([[1], [0], [1], [1]])
 
-    jarvis = Neural_Network(X,y,learning_rate = 0.4, maximal_loss = 0.000001)
+    jarvis = Neural_Network(X,y,learning_rate = 0.6, maximal_loss = 0.000001)
 
     #jarvis.tasks('train')
     jarvis.tasks('train')
